762d25c027e06abbe3cdcdbff9614190837de2f8
lucaspuchetti <lucaspuchetti@gmail.com>
2021-06-12 19:59:15 +0200

    Soporte para señales en el bloque de volumen en dwmblocks

--- config.h
+++ config.h
@@ -77,9 +77,9 @@ static const char *monitor[] = { "/usr/bin/htop", NULL };
 //sets urxvt as the default terminal
 static const char *termcmd[]  = { "kitty", NULL };
 //volume controls
-static const char *upvol[]   = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
-static const char *downvol[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
-static const char *mutevol[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
+//static const char *upvol[]   = { "amixer", "-q", "set", "Master", "5%+", "unmute", ";", "kill", "-44", "$(pidof", "dwmblocks)", NULL };
+//static const char *downvol[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", ";", "kill", "-44", "$(pidof", "dwmblocks)", NULL };
+//static const char *mutevol[] = { "amixer", "-q", "set", "Master", "toggle", ";", "kill", "-44", "$(pidof", "dwmblocks)", NULL };
 
 #include "shiftview.c"
 static char *endx[] = { "/bin/sh", "-c", "endx", "externalpipe", NULL };
@@ -113,9 +113,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,              		    XK_n,      shiftview,  	   { .i = +1 } },
 	{ MODKEY,              		    XK_b,      shiftview,      { .i = -1 } },
-    { MODKEY,                       XK_F8,     spawn,          {.v = upvol   } },
-    { MODKEY,                       XK_F7,     spawn,          {.v = downvol } },
-    { MODKEY,                       XK_F5,     spawn,          {.v = mutevol } },
+    	{ MODKEY,                       XK_F8,     spawn,          SHCMD("amixer -q set Master 5%+ unmute; kill -44 $(pidof dwmblocks)")  },
+    	{ MODKEY,                       XK_F7,     spawn,          SHCMD("amixer -q set Master 5%- unmute; kill -44 $(pidof dwmblocks)")  },
+    	{ MODKEY,                       XK_F5,     spawn,          SHCMD("amixer -q set Master toggle; kill -44 $(pidof dwmblocks)")  },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
